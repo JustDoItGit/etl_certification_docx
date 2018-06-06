e2b3201f32e5110cb81c4cb2573e879c66231e26
coolcumt <cool_kj@126.com>
2018-06-06 23:28:33 +0800

    解决列表索引越界的问题，字符串索引不越界，但是列表索引会越界

--- old_module_docx_etl.py
+++ old_module_docx_etl.py
@@ -18,7 +18,6 @@ TABLE = WORD_NAMESPACE + 'tbl'
 ROW = WORD_NAMESPACE + 'tr'
 CELL = WORD_NAMESPACE + 'tc'
 
-
 # 将传入的docx文档解析成tree结构
 def make_docx_tree(file_path):
     if file_path[-4:] != 'docx':
@@ -30,8 +29,8 @@ def make_docx_tree(file_path):
         return tree
     except Exception, e:
         # 无法解析的docx文档
+        print e
         return 66667
-
 
 # 将内容放到到三维列表里面
 def make_list_3dim(tree):
@@ -51,14 +50,13 @@ def make_list_3dim(tree):
         t += 1
     return list_3dim
 
-
 # 找到提取文档的目标内容起点所在的表格位置
 def target_positon(list_3, target_str):
     result_list = []
     for t in range(len(list_3)):
         for tr in range(len(list_3[t])):
             for td in range(len(list_3[t][tr])):
-                if target_str == list_3[t][tr][td].upper()[:len(target_str)]:
+                if target_str == list_3[t][tr][td].upper()[:len(target_str)]: # -----2018-5-7需要修改的行
                     result_list.append({'t': t, 'tr': tr, 'td': td})
     if len(result_list) == 1:
         # 返回内容所在的下标
@@ -67,7 +65,6 @@ def target_positon(list_3, target_str):
     else:
         # 文件内缺少关键参数，关键参数数量不对
         return 66668
-
 
 # 提取issued_by（发布人）
 def get_issued_by(list_3):
@@ -85,13 +82,12 @@ def get_issued_by(list_3):
     else:
         return ''
 
-
 # 提取verified_by（审核人）
 def get_verified_by(list_3):
     verified_position = target_positon(list_3, 'VERIFIED BY')
     if verified_position == 66668:
         return 66668
-    verified_by = list_3[verified_position['t']][verified_position['tr']][verified_position['td']]
+    verified_by = list_3[verified_position['t']][verified_position['tr']][verified_position['td']] # -----2018-5-7需要修改的行
     verified_people_sp = verified_by.split(':')
     if len(verified_people_sp) == 2:
         verified_by_people = verified_people_sp[1].strip()
@@ -101,7 +97,6 @@ def get_verified_by(list_3):
             return ''
     else:
         return ''
-
 
 # 提取description
 def get_description(list_3):
@@ -113,10 +108,11 @@ def get_description(list_3):
     for row in list_3[description_position['t']]:
         if len(row) >= 4 and row[1][:3] == 'DNV':
             description_list.append(row[1])
+        ##### add 2018-5-17 001 #####
         elif len(row) >= 4 and row[1][:17] == 'Reviewed document':
             description_list.append(row[1])
+        ##### add 2018-5-17 001 #####
     return description_list
-
 
 # 提取日期，并格式化为2017-12-12的格式
 def get_time(comment_str):
@@ -124,12 +120,23 @@ def get_time(comment_str):
     date_p_list = [ur'19\d{2}-[01]{0,1}\d-[0-3]{0,1}\d|20\d{2}-[01]{0,1}\d-[0-3]{0,1}\d',
                    ur'19\d{2}.[01]{0,1}\d.[0-3]{0,1}\d|20\d{2}.[01]{0,1}\d.[0-3]{0,1}\d',
                    ur'[01]{0,1}\d/[0-3]{0,1}\d/19\d{2}|[01]{0,1}\d/[0-3]{0,1}\d/20\d{2}',
-                   ur'[0-3]{0,1}\d\.[01]{0,1}\d\.19\d{2}|[0-3]{0,1}\d\.[01]{0,1}\d\.20\d{2}', ]
+                   ur'[0-3]{0,1}\d\.[01]{0,1}\d\.19\d{2}|[0-3]{0,1}\d\.[01]{0,1}\d\.20\d{2}',
+                   ]
     # 正则规则对应的时间格式化列表
-    date_str_list = ['%Y-%m-%d', '%Y.%m.%d', '%m/%d/%Y', '%d.%m.%Y', ]
+    date_str_list = ['%Y-%m-%d',
+                     '%Y.%m.%d',
+                     '%m/%d/%Y',
+                     '%d.%m.%Y',
+                     ]
     for i, p in enumerate(date_p_list):
         date_p = re.compile(p)
+        # error
+        '''
+        goldwind_p = re.compile(ur'DNVGL|Goldwind|DNV GL|DNV|gold wind', re.I)
+        date = date_p.findall(goldwind_p.split(comment_str)[0])
+        '''
         date = date_p.findall(comment_str)
+
         if date:
             t_str = date[0]
             try:
@@ -139,7 +146,6 @@ def get_time(comment_str):
                 return ''
     return ''
 
-
 # 计算两个日期时间差
 def compute_days(start_time, end_time):
     days_datetime = end_time - start_time
@@ -148,64 +154,6 @@ def compute_days(start_time, end_time):
         return days
     else:
         return 0
-
-
-def compute_loop_count(comment_content):
-    '''
-    计算loop_count（评审意见总轮次数）
-    start_time（评审意见起始时间）
-    end_time（评审意见结束时间）
-    duration（评审意见持续天数）
-    comment_content: 评审内容
-    '''
-    loop_count = 0
-    start_time = ''
-    end_time = ''
-    duration = 0
-    p = re.compile(ur'DNVGL|DNV GL|DNV', re.I)
-    p_find = p.findall(comment_content)
-    if p_find:
-        dnv_list = p.split(comment_content)[1:]
-        dnv_list_new = [i for i in dnv_list if whether_contain_date(i[:30])]
-        if len(dnv_list_new) == 0:
-            start_time = ''
-            end_time = ''
-        else:
-            loop_count = len(dnv_list_new) - 1
-            start_time = get_time(dnv_list_new[0])
-            end_time = get_time(dnv_list_new[-1])
-        if start_time != '' and end_time != '':
-            duration = compute_days(start_time, end_time)
-    if start_time == '':
-        start_time = None
-    if end_time == '':
-        end_time = None
-    return {
-        # 'comment_content': line_feed_comment_content(comment_content),  # 换行
-        'start_time': start_time,
-        'end_time': end_time,
-        'duration': duration,
-        'loop_count': loop_count,
-    }
-
-
-# 对comment_content评审意见内容换行
-def line_feed_comment_content(comment_content):
-    p = re.compile(ur'DNVGL|Goldwind|DNV GL|DNV|gold wind', re.I)
-    group_list = p.findall(comment_content)
-    line_feed = comment_content
-    if group_list:
-        say_list = p.split(comment_content)
-        say_list.remove(say_list[0])
-        if len(group_list) == len(say_list):
-            # 规整对话内容
-            say_clean = ''
-            for i, val in enumerate(group_list):
-                # \r\n\r\n用于段落间换行
-                say_clean = say_clean + val + ' ' + say_list[i] + '\r\n\r\n'
-            line_feed = say_clean
-    return line_feed
-
 
 # 用于判断每个DNV后面是否跟随时间
 def whether_contain_date(comment_str):
@@ -222,6 +170,73 @@ def whether_contain_date(comment_str):
             return True
     return False
 
+def compute_loop_count(comment_content):
+    '''
+    计算loop_count（评审意见总轮次数）
+    start_time（评审意见起始时间）
+    end_time（评审意见结束时间）
+    duration（评审意见持续天数）
+    comment_content: 评审内容
+    '''
+    loop_count = 0
+    start_time = ''
+    end_time = ''
+    duration = 0
+    '''
+    st_list = [ur'DNV GL', ur'DNVGL', ur'DNV']
+    for st in st_list:
+        p = re.compile(st, re.I)
+        p_find = p.findall(comment_content)
+        if p_find:
+            dnv_list = p.split(comment_content)[1:]
+            loop_count = len(dnv_list) - 1
+            start_time = get_time(dnv_list[0])
+            end_time = get_time(dnv_list[-1])
+            if start_time != '' and end_time != '':
+                duration = compute_days(start_time, end_time)
+            break
+    '''
+    # error
+    p = re.compile(ur'DNVGL|DNV GL|DNV', re.I)
+    p_find = p.findall(comment_content)
+    if p_find:
+        dnv_list = p.split(comment_content)[1:]
+        dnv_list_new = [i for i in dnv_list if whether_contain_date(i[:30])]
+        loop_count = len(dnv_list_new) - 1
+        start_time = get_time(dnv_list_new[0])
+        end_time = get_time(dnv_list_new[-1])
+        if start_time != '' and end_time != '':
+            duration = compute_days(start_time, end_time)
+    if start_time == '':
+        start_time = None
+    if end_time == '':
+        end_time = None
+    return {
+        # 'comment_content': line_feed_comment_content(comment_content),  # 换行
+        'start_time': start_time,
+        'end_time': end_time,
+        'duration': duration,
+        'loop_count': loop_count,
+    }
+
+# 对comment_content评审意见内容换行
+def line_feed_comment_content(comment_content):
+    p = re.compile(ur'DNVGL|Goldwind|DNV GL|DNV|gold wind', re.I)
+    group_list = p.findall(comment_content)
+    line_feed = comment_content
+    if group_list:
+        say_list = p.split(comment_content)
+        say_list.remove(say_list[0])
+        if len(group_list) == len(say_list):
+            # 规整对话内容
+            say_clean = ''
+            for i, val in enumerate(group_list):
+                # \r\n\r\n用于段落间换行
+                ##### add 2018-5-17 002 是否换行的判断 #####
+                say_clean = say_clean + val + ' ' + say_list[i] + '\r\n\r\n'
+                ##### add 2018-5-17 002 是否换行的判断 #####
+            line_feed = say_clean
+    return line_feed
 
 # main etl主函数
 def etl_docx(file_path):
@@ -256,4 +271,8 @@ def etl_docx(file_path):
         'comment_all': comment_all,
     }
 
-# print(etl_docx('load validation_VCS-00985-A056-001_ rev 2.docx'))
+'''
+66666: u'文档后缀不是docx，无法解析',
+66667: u'无法解析的docx文档',
+66668: u'文档中缺少关键参数VERIFICATION COMMENTS、issued by、verified by',
+'''
--- test_etl_docx.py
+++ test_etl_docx.py
@@ -1,30 +1,30 @@
 # coding:utf-8
-from etl_certification_docx import module_docx_etl
+import module_docx_etl
 import traverse_files_name_2_list
 import time
 
 start_time = time.time()
 
-file_path = '../file_position/Manuals PD-PP039140-17J3421-20.VCS.rev04.docx'
-result = module_docx_etl.etl_docx(file_path)
-print result
+# file_path = '../file_position/Manuals PD-PP039140-17J3421-20.VCS.rev04.docx'
+# result = module_docx_etl.etl_docx(file_path)
+# print result
 
 
-'''
-file_name_l = traverse_files_name_2_list.file_name_list('../file_position/GW 121-2500 IEC IIIB Sinoma59.5 HH90/',
+
+file_name_l = traverse_files_name_2_list.file_name_list('../file_position/GW121-2500_Sinoma59.5_120m_IIIB',
                                                         'docx')
 print(file_name_l)
 import time
 
 start_time = time.time()
 for i in file_name_l:
-    path = '../file_position/GW 121-2500 IEC IIIB Sinoma59.5 HH90/' + i
+    path = '../file_position/GW121-2500_Sinoma59.5_120m_IIIB/' + i
     l = module_docx_etl.etl_docx(path)
     print(l)
-    print i
+    # print i
 end_time = time.time()
 print('cost time: %f' % (end_time - start_time))
-'''
+
 
 end_time = time.time()
 print('cost time: %fs' %(end_time -start_time))
